0b43b20888fe76bc3075c1535e1e287683b4ef06
Linda <ls18100@edu.rta.lv>
2020-02-18 11:10:43 +0200

    rest_api

--- lib/events.dart
+++ lib/events.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-
+import 'dart:async';
 import 'package:flutter/material.dart';
+
+
+
 
 class EventsPage extends StatefulWidget {
   @override
@@ -11,48 +14,204 @@ class EventsPage extends StatefulWidget {
 
 class _EventsPageState extends State<EventsPage> {
   @override
+  final String url = "http://vis.rta.lv/service/roomschedule/test";
+  List data;
+
+  Future<String> getSWData() async {
+    var res =
+    await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var resBody = json.decode(res.body);
+      data = resBody["results"];
+    });
+
+    return "Success!";
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Events Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Events page"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Sarakts: "),
+                            Text(data[index]["sarakts"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Id: "),
+                            Text(data[index]["id"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.red)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Telpa: "),
+                            Text(data[index]["telpa"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Laiks: "),
+                            Text(data[index]["laiks"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Laiks: "),
+                            Text(data[index]["laiks"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Izmainas: "),
+                            Text(data[index]["izmainas"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Statuss: "),
+                            Text(data[index]["statuss"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Kurss: "),
+                            Text(data[index]["kurss"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Kursa_kods: "),
+                            Text(data[index]["kursa_kods"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Datums: "),
+                            Text(data[index]["datums"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Prof_title: "),
+                            Text(data[index]["prof_title"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Name: "),
+                            Text(data[index]["name"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                  Card(
+                    child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("Surname: "),
+                            Text(data[index]["surname"],
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.black87)),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
-}
 
-class URLS {
-  static const String BASE_URL = 'http://vis.rta.lv/service/roomschedule/test';
-}
-
-class ApiService {
-  static Future<List<dynamic>> getEmployees() async {
-    // RESPONSE JSON :
-    // [{
-    //   "id": "1",
-    //   "employee_name": "",
-    //   "employee_salary": "0",
-    //   "employee_age": "0",
-    //   "profile_image": ""
-    // }]
-    final response = await http.get('${URLS.BASE_URL}');
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      return null;
-    }
-  }
-
-  static Future<bool> addEmployee(body) async {
-    // BODY
-    // {
-    //   "name": "test",
-    //   "age": "23"
-    // }
-    final response = await http.post('${URLS.BASE_URL}/create', body: body);
-    if (response.statusCode == 200) {
-      return true;
-    } else {
-      return false;
-    }
+  @override
+  void initState() {
+    super.initState();
+    this.getSWData();
   }
 }
